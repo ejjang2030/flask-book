22b02bfcec3672d5c808e27352964e6d747bd54e
thinkodia-omen\thinkodia <ejjang2030@gmail.com>
2026-04-14 21:47:42 +0900

    feat : chapter 01 완료

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,7 +1,24 @@
-from flask import Flask, current_app, g, render_template, request, url_for
+import logging
+import os
 
-# flask 클래스를 인스턴스화한다
+from email_validator import EmailNotValidError, validate_email
+from flask import (Flask, current_app, flash, g, make_response, redirect,
+                   render_template, request, session, url_for)
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_mail import Mail, Message
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "2AZSMss3p5QPbcY2hBsJ"
+app.logger.setLevel(logging.DEBUG)
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+toolbar = DebugToolbarExtension(app)
+app.config["MAIL_SERVER"] = os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
+app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
+mail = Mail(app)
 
 
 @app.route('/')
@@ -27,6 +44,69 @@ def hello_name(name):
 def show_name(name):
     # 변수를 템플릿 엔진에게 건넨다
     return render_template('index.html', name=name)
+
+
+@app.route("/contact")
+def contact():
+    response = make_response(render_template('contact.html'))
+    response.set_cookie('flaskbook key', 'flaskbook value')
+    session['username'] = 'AK'
+    return render_template('contact.html')
+
+
+@app.route('/contact/complete', methods=['GET', 'POST'])
+def contact_complete():
+    if request.method == 'POST':
+        # form 속성을 사용해서 폼 값을 취득한다
+        username = request.form['username']
+        email = request.form['email']
+        description = request.form['description']
+
+        # 입력 체크
+        is_valid = True
+
+        if not username:
+            flash('사용자명은 필수입니다')
+            is_valid = False
+        
+        if not email:
+            flash('메일 주소는 필수입니다')
+            is_valid = False
+        
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash('메일 주소의 형식으로 입력해 주세요')
+            is_valid = False
+        
+        if not description:
+            flash('문의 내용은 필수입니다')
+            is_valid = False
+        
+        if not is_valid:
+            return redirect(url_for('contact'))
+
+        # 이메일을 보낸다(나중에 구현할 부분)
+        send_email(
+            email,
+            "문의 감사합니다.",
+            'contact_mail',
+            username=username,
+            description=description,
+        )
+
+        # contact 엔드포인트로 리다이렉트한다
+        flash('문의 내용은 메일로 송신했습니다. 문의해 주셔서 감사합니다.')
+        return redirect(url_for('contact_complete'))
+    return render_template('contact_complete.html')
+
+
+def send_email(to, subject, template, **kwargs):
+    """메일을 송신하는 함수"""
+    msg = Message(subject, recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
 
 
 with app.test_request_context():
