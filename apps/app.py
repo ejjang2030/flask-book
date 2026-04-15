48ba9aa71466f522e0e01da4ba090c8721a9ba36
320-18\thinkodia <ejjang2030@gmail.com>
2026-04-15 17:50:26 +0900

    feat : ch2 완료

---
+++ apps/app.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
+
+from apps.config import config
+
+db = SQLAlchemy()
+csrf = CSRFProtect()
+
+
+def create_app(config_key):
+    app = Flask(__name__)
+
+    app.config.from_object(config[config_key])
+
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get('FLASK_SECRET_KEY'),
+        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+        SQLALCHEMY_TRACK_MODIFICATIONS=False,
+        SQLALCHEMY_ECHO=True,
+        WTF_CSRF_SECRET_KEY=os.environ.get("FLASK_SECRET_KEY"),
+    )
+
+    csrf.init_app(app)
+    db.init_app(app)
+    Migrate(app, db)
+
+    from apps.crud import views as crud_views
+    app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    return app
